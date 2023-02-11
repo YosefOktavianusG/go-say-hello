02da85055764f8b0a856738a7ea1a320a9c3edc3
yosef oktavianus <oktavianus_yosef@hotmail.com>
2023-02-11 17:22:53 +0700

    tes v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/YosefOktavianusG/go-say-hello
+module github.com/YosefOktavianusG/go-say-hello/v2
 
 go 1.20
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package gosayhello
 
-func SayHello() string {
-	return "Helloo"
+func SayHello(name string) string {
+	return "Hello " + name
 }
